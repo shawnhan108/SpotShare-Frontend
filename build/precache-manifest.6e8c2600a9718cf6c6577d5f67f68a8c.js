00cb7288f696de8d608f595ef72d3b4613dcfdd7
SHAWN <shawnhan1999@hotmail.com>
2019-09-10 01:15:28 -0400

    ready for deployment

---
+++ build/precache-manifest.6e8c2600a9718cf6c6577d5f67f68a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee2e0da9815bc59744c0c2967efc7d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10ac81b86670459e0194",
+    "url": "/static/css/2.e303647c.chunk.css"
+  },
+  {
+    "revision": "b1fd033f59125db24f0b",
+    "url": "/static/css/main.c797b0ea.chunk.css"
+  },
+  {
+    "revision": "10ac81b86670459e0194",
+    "url": "/static/js/2.c4982fd5.chunk.js"
+  },
+  {
+    "revision": "b1fd033f59125db24f0b",
+    "url": "/static/js/main.ab462fac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
